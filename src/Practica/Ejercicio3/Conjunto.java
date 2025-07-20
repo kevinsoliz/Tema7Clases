b31a91b59662801b609a52be0556c1894ea5f72b
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-20 17:09:16 +0200

    Ejercicio3: hay problemas con la verificación de si el conjunto es igual a otro

---
+++ src/Practica/Ejercicio3/Conjunto.java
@@ -0,0 +1,86 @@
+package Practica.Ejercicio3;
+
+import Actividades.Lista;
+
+public class Conjunto {
+    private Lista conjunto;
+
+    public Conjunto(){
+        conjunto = new Lista();
+    }
+
+    public int numeroElementos(){
+        return conjunto.getNumElementos();
+    }
+
+    public boolean insertar(Integer nuevo){
+        boolean insertado = false;
+        if(!pertenece(nuevo)){
+            conjunto.insertarFinal(nuevo);
+            insertado = true;
+        }
+       return insertado;
+    }
+
+    public boolean insertar(Conjunto otroConjunto){
+        boolean insertado = false;
+        int longitudAnterior = numeroElementos();
+        if(!iguales(otroConjunto)) {
+            for (int i = 0; i < otroConjunto.numeroElementos(); i++) {
+                insertar(otroConjunto.conjunto.obtenerElemento((byte) i));
+            }
+            insertado = true;
+        }
+        return insertado;
+
+    }
+
+    public boolean eliminarElemento(Integer elemento){
+        boolean eliminado = false;
+        if(pertenece(elemento)){
+            int indice = conjunto.obtenerIndice(elemento);
+            conjunto.eliminarElemento(indice);
+            eliminado = true;
+        }
+        return eliminado;
+    }
+
+    public boolean eliminarConjunto(Conjunto otroConjunto){
+        boolean eliminado = false;
+        if(!iguales(otroConjunto)) {
+            for (int i = 0; i < otroConjunto.numeroElementos(); i++) {
+                eliminarElemento(otroConjunto.conjunto.obtenerElemento((byte) i));
+            }
+            eliminado = true;
+        }
+        return eliminado;
+    }
+
+    public boolean pertenece(Integer elemento){
+        boolean existe = false;
+        for(int i = 0; i < numeroElementos(); i++){
+            if(conjunto.obtenerElemento((byte)i) == elemento)
+                existe = true;
+        }
+        return existe;
+    }
+
+    public void muestra(){
+        System.out.println("Conjunto: " + conjunto.toString());
+    }
+
+    public boolean iguales(Conjunto otroConjunto){
+        boolean iguales = false;
+        int contador = 0;
+ //       if(numeroElementos() == otroConjunto.numeroElementos()){
+            while((contador < otroConjunto.numeroElementos()) && pertenece(otroConjunto.conjunto.obtenerElemento((byte)contador))){
+                contador++;
+            }
+            if(contador == otroConjunto.numeroElementos())
+                iguales = true;
+//        }
+
+
+        return iguales;
+    }
+}
---
+++ src/Practica/Ejercicio3/Main.java
@@ -0,0 +1,26 @@
+package Practica.Ejercicio3;
+
+public class Main {
+    public static void main(String[] args) {
+        var conjunto = new Conjunto();
+        conjunto.insertar(3);
+        conjunto.insertar(4);
+        conjunto.insertar(5);
+        conjunto.insertar(6);
+        conjunto.insertar(7);
+        conjunto.muestra();
+        var conjunto2 = new Conjunto();
+        conjunto2.insertar(2);
+        conjunto2.insertar(33);
+        conjunto2.insertar(55);
+        conjunto2.insertar(66);
+        conjunto2.insertar(77);
+
+        conjunto.insertar(conjunto2);
+        conjunto.muestra();
+        System.out.println("Eliminar");
+        System.out.println(conjunto.iguales(conjunto2));
+        conjunto.muestra();
+
+    }
+}
